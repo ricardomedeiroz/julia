98054919785fc9c11829c4e5a9bba20d7517916c
Ricardo Medeiros Ribeiro <ricardomedeiros116@gmail.com>
2026-03-27 01:16:13 -0300

    Fix onload event to remove 'container' class
    Add window.onload to remove 'container' class after page load.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-onload = () =>{
-  document.body.classList.remove("container");
+window.onload = () => {
+    // Quando a página 2 terminar de carregar, isso remove a classe que pausa a animação
+    document.body.classList.remove("container");
 };
